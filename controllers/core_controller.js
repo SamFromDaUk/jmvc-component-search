30aa57f5b5013149f814c1bf1350478d26097d55
David Hodgkinson <david.hodgkinson@frogtrade.com>
2013-05-31 14:17:41 +0100

    Search Component - Refactored event listeners in core controller and enhanced interface #CS

--- controllers/core_controller.js
+++ controllers/core_controller.js
@@ -1,38 +1,45 @@
 Frog.Controller.extend('Frogui.Controllers.Components.Search', {
-    /* @Static */
-    listensTo: [],
 
 }, {
-    search: null,
+    currentSearchTimeout: null,
 
     init: function() {
+        this.options.minimum_char = this.options.minimum_char || 3;
+        this.options.search_delay = this.options.search_delay || 1000;
+        this.options.template = this.options.template || '//frogui/components/search/views/core/search.ejs';
         this.model = new Frogui.Models.Components.Search.Data();
         this.render();
     },
 
     render: function() {
-        this.minimum_char = this.options.minimum_char || 0;
-        this.options.template = this.options.template || '//frogui/components/search/views/core/search.ejs';
-        this.search_delay = this.options.search_delay || 1000;
         this.element.html(this.view(this.options.template, this.options));
+    },
+    
+    setSearchTimeout: function() {
+        var self = this,
+            needle = this.model.attr('needle');
+        
+        window.clearTimeout(this.currentSearchTimeout);
+
+        // Prevent short search terms so we don't fetch huge result sets. Allow empty search to reset to all results
+        if (needle.length >= this.options.minimum_char || !needle) {
+            this.currentSearchTimeout = setTimeout(function() {
+                self.element.trigger('search.find', needle);
+            }, this.options.search_delay);
+        }
+    },
+
+    "form submit": function(el, ev) {
+        // Prevent default form submit behaviour
+        return false;
     },
 
     "input keyup": function(el, ev) {
-        var self = this;
         this.model.attr('needle', el.val().replace(/^\s+|\s+$/, ''));
-
-        //Get rid of any previous timeouts.
-        window.clearTimeout(this.search);
-
-        if (this.model.attr('needle').length > this.minimum_char) {
-            //Setup a delay of 1000ms so that a user can type without loading on every keyup
-            this.search = setTimeout(function() {
-                self.element.trigger('search.find', self.model.attr('needle'));
-            }, 1000);
-        }
+        this.setSearchTimeout();
     },
+
     "search.getNeedle": function(el, ev, callback) {
         callback(this.model.attr('needle'));
     }
-
 });
